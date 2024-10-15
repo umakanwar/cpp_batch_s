6c67f83581ae3ab151ea54ef9db9515ddd6d5890
umakanwar <kanwaruma298@gmail.com>
2024-10-16 04:21:17 +0530

    Add files via upload

---
+++ 24 september/Q4.cpp
@@ -0,0 +1,13 @@
+// 4. 1 2 4 8 32 ....
+//      a=1 b=2 c= power of 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int c;
+    for(int i=0;i<10;i++){
+        c=pow(2,i);
+        cout<<c<<" ";
+    }
+}
